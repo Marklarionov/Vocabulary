eda9925be851165220d097c1ca4fa209dde6fbb7
Marklarionov <93324243+Marklarionov@users.noreply.github.com>
2022-01-25 22:04:41 +0200

    Update Vocabulary.py

--- Vocabulary.py
+++ Vocabulary.py
@@ -4,26 +4,32 @@ engl=[]
 rusl=failist_lugemine("rus.txt",rusl)
 engl=failist_lugemine("ang.txt",engl)
 print("This is a Russian-English and English-Russian dictionary.")
-
 while True:
-    print("Choose what you want to do")
-    print("1 - ")
-    print("2 - to choose a Russian-English dictionary")
-    print("3 - to choose a English-Russian dictionary")
-    print("4 - Add new word to dictionary")
-    print("5 - To exit the dictionary")
+    print("""Choose what you want to do")
+    1 - Show all words")
+    2 - To choose a Russian-English dictionary")
+    3 - To choose a English-Russian dictionary")
+    4 - Add new word to dictionary")
+    5 - Correct mistake")
+    6 - To exit the dictionary")""")
     menu=int(input("Your choise: "))
     if menu==1:
         print(rusl)
         print(engl)
     elif menu==2:
-        print()
+        word= input("Choose the word you want to translate: ")
+        indx = rusl.index(word)
+        print(f"{word} - {engl[indx]}")
     elif menu==3:
-        print()
+        word= input("Choose the word you want to translate: ")
+        indeks = engl.index(word)
+        print(f"{word} - {rusl[indx]}")
     elif menu==4:
         rusl=add_word("rus.txt",input("Новое слово: "))
         engl=add_word("ang.txt",input("New word: "))
     elif menu==5:
         print()
+    elif menu==6:
+        exit()
     else:
         print("Incorrect Value")
